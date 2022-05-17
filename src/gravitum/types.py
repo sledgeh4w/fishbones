d75c6040e7b8fdbc431708769d6ab426fab27015
Sh4ww <sh4w0911@gmail.com>
2022-05-17 20:06:57 +0800

    Format code with yapf

--- src/gravitum/types.py
+++ src/gravitum/types.py
@@ -3,30 +3,25 @@ from typing import Union, Type, SupportsBytes, Iterable
 
 import numpy as np
 
-# Operation methods to be overrode.
-_OVERRIDE_OPERATIONS = [
-    '__neg__', '__pos__', '__invert__',
-    '__add__', '__radd__', '__iadd__',
-    '__sub__', '__rsub__', '__isub__',
-    '__mul__', '__rmul__', '__imul__',
-    '__truediv__', '__rtruediv__', '__itruediv__',
-    '__floordiv__', '__rfloordiv__', '__ifloordiv__',
-    '__mod__', '__rmod__', '__imod__',
-    '__and__', '__rand__', '__iand__',
-    '__or__', '__ror__', '__ior__',
-    '__xor__', '__rxor__', '__ixor__',
-    '__lshift__', '__rlshift__', '__ilshift__',
-    '__rshift__', '__rrshift__', '__irshift__'
-]
-
 
 class IntMeta(type):
     """Meta class of type int."""
 
+    # Operation methods to be overrode.
+    _OVERRIDE_OPERATIONS = [
+        '__neg__', '__pos__', '__invert__', '__add__', '__radd__', '__iadd__',
+        '__sub__', '__rsub__', '__isub__', '__mul__', '__rmul__', '__imul__',
+        '__truediv__', '__rtruediv__', '__itruediv__', '__floordiv__',
+        '__rfloordiv__', '__ifloordiv__', '__mod__', '__rmod__', '__imod__',
+        '__and__', '__rand__', '__iand__', '__or__', '__ror__', '__ior__',
+        '__xor__', '__rxor__', '__ixor__', '__lshift__', '__rlshift__',
+        '__ilshift__', '__rshift__', '__rrshift__', '__irshift__'
+    ]
+
     def __init__(cls, name, bases, attr_dict):
         super().__init__(name, bases, attr_dict)
 
-        for op in _OVERRIDE_OPERATIONS:
+        for op in cls._OVERRIDE_OPERATIONS:
             if not hasattr(cls, op):
                 continue
             setattr(cls, op, cls.operation_wrapper(getattr(cls, op)))
@@ -53,14 +48,13 @@ class IntMeta(type):
 
                 elif isinstance(self, np.integer):
                     if self.itemsize == other.itemsize:
-                        data_type = (type(other)
-                                     if isinstance(self, np.signedinteger)
-                                     else type(self))
+                        data_type = (type(other) if isinstance(
+                            self, np.signedinteger) else type(self))
 
                     else:
                         data_type = (type(other)
-                                     if self.itemsize < other.itemsize
-                                     else type(self))
+                                     if self.itemsize < other.itemsize else
+                                     type(self))
 
                 return data_type(f(self, other))
 
@@ -83,25 +77,18 @@ class IntMixin(np.integer):
         return issubclass(cls, np.signedinteger)
 
     @classmethod
-    def from_bytes(
-            cls,
-            data: Union[Iterable[int], SupportsBytes],
-            byteorder: str = 'little'
-    ):
+    def from_bytes(cls,
+                   data: Union[Iterable[int], SupportsBytes],
+                   byteorder: str = 'little'):
         """Return a value of this type from given bytes"""
-        return cls(int.from_bytes(
-            data,
-            byteorder=byteorder,
-            signed=cls.get_signed()
-        ))
+        return cls(
+            int.from_bytes(data, byteorder=byteorder, signed=cls.get_signed()))
 
     def to_bytes(self, byteorder: str = 'little') -> bytes:
         """Covert this value to bytes."""
-        return int(self).to_bytes(
-            length=self.get_size(),
-            byteorder=byteorder,
-            signed=self.get_signed()
-        )
+        return int(self).to_bytes(length=self.get_size(),
+                                  byteorder=byteorder,
+                                  signed=self.get_signed())
 
 
 class Int8(np.int8, IntMixin, metaclass=IntMeta):
@@ -144,15 +131,9 @@ class UInt64(np.uint64, IntMixin, metaclass=IntMeta):
     _SIZE = 8
 
 
-IntVar = Union[
-    Int8, Int16, Int32, Int64,
-    UInt8, UInt16, UInt32, UInt64
-]
+IntVar = Union[Int8, Int16, Int32, Int64, UInt8, UInt16, UInt32, UInt64]
 
-IntType = Type[Union[
-    Int8, Int16, Int32, Int64,
-    UInt8, UInt16, UInt32, UInt64
-]]
+IntType = Type[Union[Int8, Int16, Int32, Int64, UInt8, UInt16, UInt32, UInt64]]
 
 
 def int8(v) -> Int8:
--- src/gravitum/utils.py
+++ src/gravitum/utils.py
@@ -1,15 +1,13 @@
 import re
 from typing import Optional
 
-from .types import (
-    Int8, Int16, Int32, Int64, UInt8, UInt16, UInt32, UInt64, IntType)
+from .types import (Int8, Int16, Int32, Int64, UInt8, UInt16, UInt32, UInt64,
+                    IntType)
 
 
-def get_type(
-        size: Optional[int] = None,
-        signed: Optional[bool] = None,
-        type_name: Optional[str] = None
-) -> IntType:
+def get_type(size: Optional[int] = None,
+             signed: Optional[bool] = None,
+             type_name: Optional[str] = None) -> IntType:
     """Get type int with specified size and signed."""
     if type_name is not None:
         match = re.match(r'(u*)int(\d+)', type_name)
@@ -23,14 +21,10 @@ def get_type(
         signed = not bool(match.group(1))
         size = nbits // 8
 
-    int_types = [
-        Int8, Int16, Int32, Int64,
-        UInt8, UInt16, UInt32, UInt64,
-    ]
+    int_types = [Int8, Int16, Int32, Int64, UInt8, UInt16, UInt32, UInt64]
 
     for int_type in int_types:
-        if (int_type.get_size() == size and
-                int_type.get_signed() == signed):
+        if int_type.get_size() == size and int_type.get_signed() == signed:
             return int_type
 
     raise NotImplementedError()
