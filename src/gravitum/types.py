e8d5405ad4a75fdcd7e75308e5549eafed60980c
Sh4ww <sh4w0911@gmail.com>
2022-05-18 14:53:40 +0800

    Refactor types

--- src/gravitum/types.py
+++ src/gravitum/types.py
@@ -1,3 +1,4 @@
+import re
 from functools import wraps
 from typing import Union, Type, SupportsBytes, Iterable
 
@@ -5,7 +6,7 @@ import numpy as np
 
 
 class IntMeta(type):
-    """Meta class of type int."""
+    """Meta class of integer type."""
 
     # Operation methods to be overrode.
     _OVERRIDE_OPERATIONS = [
@@ -20,6 +21,9 @@ class IntMeta(type):
 
     def __init__(cls, name, bases, attr_dict):
         super().__init__(name, bases, attr_dict)
+
+        size = int(re.match(r'(U*)Int(\d+)', cls.__name__).group(2)) // 8
+        setattr(cls, '_SIZE', size)
 
         for op in cls._OVERRIDE_OPERATIONS:
             if not hasattr(cls, op):
@@ -63,8 +67,8 @@ class IntMeta(type):
         return decorator
 
 
-class IntMixin(np.integer):
-    """Mixin class of type int."""
+class IntBase(np.integer):
+    """Base class of integer type."""
 
     @classmethod
     def get_size(cls) -> int:
@@ -91,44 +95,36 @@ class IntMixin(np.integer):
                                   signed=self.get_signed())
 
 
-class Int8(np.int8, IntMixin, metaclass=IntMeta):
+class Int8(IntBase, np.int8, metaclass=IntMeta):
     """Int8"""
-    _SIZE = 1
 
 
-class Int16(np.int16, IntMixin, metaclass=IntMeta):
+class Int16(IntBase, np.int16, metaclass=IntMeta):
     """Int16"""
-    _SIZE = 2
 
 
-class Int32(np.int32, IntMixin, metaclass=IntMeta):
+class Int32(IntBase, np.int32, metaclass=IntMeta):
     """Int32"""
-    _SIZE = 4
 
 
-class Int64(np.int64, IntMixin, metaclass=IntMeta):
+class Int64(IntBase, np.int64, metaclass=IntMeta):
     """Int64"""
-    _SIZE = 8
 
 
-class UInt8(np.uint8, IntMixin, metaclass=IntMeta):
+class UInt8(IntBase, np.uint8, metaclass=IntMeta):
     """UInt8"""
-    _SIZE = 1
 
 
-class UInt16(np.uint16, IntMixin, metaclass=IntMeta):
+class UInt16(IntBase, np.uint16, metaclass=IntMeta):
     """UInt16"""
-    _SIZE = 2
 
 
-class UInt32(np.uint32, IntMixin, metaclass=IntMeta):
+class UInt32(IntBase, np.uint32, metaclass=IntMeta):
     """UInt32"""
-    _SIZE = 4
 
 
-class UInt64(np.uint64, IntMixin, metaclass=IntMeta):
+class UInt64(IntBase, np.uint64, metaclass=IntMeta):
     """UInt64"""
-    _SIZE = 8
 
 
 IntVar = Union[Int8, Int16, Int32, Int64, UInt8, UInt16, UInt32, UInt64]
@@ -137,40 +133,40 @@ IntType = Type[Union[Int8, Int16, Int32, Int64, UInt8, UInt16, UInt32, UInt64]]
 
 
 def int8(v) -> Int8:
-    """Short-hand for `Int8(v)`."""
+    """Shorthand for `Int8(v)`."""
     return Int8(v)
 
 
 def int16(v) -> Int16:
-    """Short-hand for `Int16(v)`."""
+    """Shorthand for `Int16(v)`."""
     return Int16(v)
 
 
 def int32(v) -> Int32:
-    """Short-hand for `Int32(v)`."""
+    """Shorthand for `Int32(v)`."""
     return Int32(v)
 
 
 def int64(v) -> Int64:
-    """Short-hand for `Int64(v)`."""
+    """Shorthand for `Int64(v)`."""
     return Int64(v)
 
 
 def uint8(v) -> UInt8:
-    """Short-hand for `UInt8(v)`."""
+    """Shorthand for `UInt8(v)`."""
     return UInt8(v)
 
 
 def uint16(v) -> UInt16:
-    """Short-hand for `UInt16(v)`."""
+    """Shorthand for `UInt16(v)`."""
     return UInt16(v)
 
 
 def uint32(v) -> UInt32:
-    """Short-hand for `UInt32(v)`."""
+    """Shorthand for `UInt32(v)`."""
     return UInt32(v)
 
 
 def uint64(v) -> UInt64:
-    """Short-hand for `UInt64(v)`."""
+    """Shorthand for `UInt64(v)`."""
     return UInt64(v)
