0112fc04178642c12d6d9bdeabf6c0a32b58a317
Sh4w <sh4w0911@gmail.com>
2022-11-15 18:45:30 +0800

    Move common functions to utils

--- src/gravitum/decompiler_builtins/ghidra.py
+++ src/gravitum/decompiler_builtins/ghidra.py
@@ -1,116 +1,100 @@
 """Implement functions which are used in the code decompiled by Ghidra."""
 
-from typing import Type, TypeVar
-
-from ..integer import IntVar, int8, int16, int32, int64, uint8, uint16, uint32, uint64
-from ..utils import get_type
-from .ida import _truncate, cfadd, ofsub, ofadd
+from ..integer import int8, int16, int32, int64, uint8, uint16, uint32, uint64
+from ..utils import truncate, zero_extend, sign_extend
+from .ida import cfadd, ofsub, ofadd
 
 # Refer to https://github.com/NationalSecurityAgency/ghidra/blob/master/Ghidra/Features/Decompiler/src/main/help/help/topics/DecompilePlugin/DecompilerConcepts.html    # noqa: E501
-
-_T = TypeVar("_T", int8, int16, int32, int64, uint8, uint16, uint32, uint64)
-
-
-def _zero_extend(x: IntVar, to_type: Type[_T]) -> _T:
-    """Zero extend."""
-    return to_type.from_bytes(x.to_bytes())
-
-
-def _sign_extend(x: IntVar, to_type: Type[_T]) -> _T:
-    """Sign extend."""
-    t1 = get_type(size=x.get_size(), signed=True)
-    t2 = get_type(size=to_type.get_size(), signed=True)
-    return to_type.from_bytes(t2.from_bytes(t1(x).to_bytes()).to_bytes())
 
 
 def sub21(x: uint16, c: int) -> uint8:
     """Implementation of `SUB21`."""
-    return _truncate(x, c, uint8)
+    return truncate(x, c, uint8)
 
 
 def sub41(x: uint32, c: int) -> uint8:
     """Implementation of `SUB41`."""
-    return _truncate(x, c, uint8)
+    return truncate(x, c, uint8)
 
 
 def sub42(x: uint32, c: int) -> uint16:
     """Implementation of `SUB42`."""
-    return _truncate(x, c, uint16)
+    return truncate(x, c, uint16)
 
 
 def sub81(x: uint64, c: int) -> uint8:
     """Implementation of `SUB81`."""
-    return _truncate(x, c, uint8)
+    return truncate(x, c, uint8)
 
 
 def sub82(x: uint64, c: int) -> uint16:
     """Implementation of `SUB82`."""
-    return _truncate(x, c, uint16)
+    return truncate(x, c, uint16)
 
 
 def sub84(x: uint64, c: int) -> uint32:
     """Implementation of `SUB84`."""
-    return _truncate(x, c, uint32)
+    return truncate(x, c, uint32)
 
 
 def zext12(x: uint8) -> uint16:
     """Implementation of `ZEXT12`."""
-    return _zero_extend(x, uint16)
+    return zero_extend(x, uint16)
 
 
 def zext14(x: uint8) -> uint32:
     """Implementation of `ZEXT14`."""
-    return _zero_extend(x, uint32)
+    return zero_extend(x, uint32)
 
 
 def zext18(x: uint8) -> uint64:
     """Implementation of `ZEXT18`."""
-    return _zero_extend(x, uint64)
+    return zero_extend(x, uint64)
 
 
 def zext24(x: uint16) -> uint32:
     """Implementation of `ZEXT24`."""
-    return _zero_extend(x, uint32)
+    return zero_extend(x, uint32)
 
 
 def zext28(x: uint16) -> uint64:
     """Implementation of `ZEXT28`."""
-    return _zero_extend(x, uint64)
+    return zero_extend(x, uint64)
 
 
 def zext48(x: uint32) -> uint64:
     """Implementation of `ZEXT48`."""
-    return _zero_extend(x, uint64)
+    return zero_extend(x, uint64)
 
 
 def sext12(x: uint8) -> uint16:
     """Implementation of `SEXT12`."""
-    return _sign_extend(x, uint16)
+    return sign_extend(x, uint16)
 
 
 def sext14(x: uint8) -> uint32:
     """Implementation of `SEXT14`."""
-    return _sign_extend(x, uint32)
+    return sign_extend(x, uint32)
 
 
 def sext18(x: uint8) -> uint64:
     """Implementation of `SEXT18`."""
-    return _sign_extend(x, uint64)
+    return sign_extend(x, uint64)
 
 
 def sext24(x: uint16) -> uint32:
     """Implementation of `SEXT24`."""
-    return _sign_extend(x, uint32)
+    return sign_extend(x, uint32)
 
 
 def sext28(x: uint16) -> uint64:
     """Implementation of `SEXT28`."""
-    return _sign_extend(x, uint64)
+    return sign_extend(x, uint64)
 
 
 def sext48(x: uint32) -> uint64:
     """Implementation of `SEXT48`."""
-    return _sign_extend(x, uint64)
+    return sign_extend(x, uint64)
 
 
 def sborrow1(x: uint8, y: uint8) -> int:
